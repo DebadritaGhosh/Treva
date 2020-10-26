c8db4873259d4acf95000678597e1b9ebd58e750
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2020-10-26 14:59:14 +0530

    Updates

---
+++ precache-manifest.5ef80d7572bc0b50ec381083b157006c.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640485dcde5e2b0a2eeec52c045bf426",
+    "url": "/treva-firstReactWebsite/index.html"
+  },
+  {
+    "revision": "daf4986d30abada907cd",
+    "url": "/treva-firstReactWebsite/static/css/main.d6ac7a12.chunk.css"
+  },
+  {
+    "revision": "59d4712f9dba824df3f6",
+    "url": "/treva-firstReactWebsite/static/js/2.e7755da0.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/treva-firstReactWebsite/static/js/2.e7755da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf4986d30abada907cd",
+    "url": "/treva-firstReactWebsite/static/js/main.1207982a.chunk.js"
+  },
+  {
+    "revision": "4830270bce1a7425718f",
+    "url": "/treva-firstReactWebsite/static/js/runtime-main.4aeabb05.js"
+  },
+  {
+    "revision": "2798d2aa7d8b962237166fc9a7f2b4cf",
+    "url": "/treva-firstReactWebsite/static/media/agra.2798d2aa.jpg"
+  },
+  {
+    "revision": "4b2c539f754ec1918ddcde7b36174aec",
+    "url": "/treva-firstReactWebsite/static/media/andaman.4b2c539f.jpg"
+  },
+  {
+    "revision": "0c7e04d3e88c68c0641e8c571ca734b0",
+    "url": "/treva-firstReactWebsite/static/media/bali.0c7e04d3.jpg"
+  },
+  {
+    "revision": "46c234a410250b670ba3b7caa21dc21e",
+    "url": "/treva-firstReactWebsite/static/media/bus.46c234a4.svg"
+  },
+  {
+    "revision": "8f6c18a488955c2f9904f65bf02e8de9",
+    "url": "/treva-firstReactWebsite/static/media/chair.8f6c18a4.svg"
+  },
+  {
+    "revision": "4a8b9aac5d7ec6742b374b2d7b9d6bd1",
+    "url": "/treva-firstReactWebsite/static/media/darjeeling.4a8b9aac.jpg"
+  },
+  {
+    "revision": "cc6cb01286eadb097551868937d2eb50",
+    "url": "/treva-firstReactWebsite/static/media/dubai.cc6cb012.jpg"
+  },
+  {
+    "revision": "8151a85880fa3fcfa1772dff28b3e522",
+    "url": "/treva-firstReactWebsite/static/media/food.8151a858.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/treva-firstReactWebsite/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "e178d5c581dc661dc071ecef51bdef7a",
+    "url": "/treva-firstReactWebsite/static/media/goa.e178d5c5.jpg"
+  },
+  {
+    "revision": "bb24b877a6d817f94ccc2173ebfa0bec",
+    "url": "/treva-firstReactWebsite/static/media/greece.bb24b877.jpg"
+  },
+  {
+    "revision": "ee3dac59ba5a1aea3ce172b25d7a8549",
+    "url": "/treva-firstReactWebsite/static/media/himachal.ee3dac59.jpg"
+  },
+  {
+    "revision": "6b1f54a4e4b6b8bbb79345a9274178d0",
+    "url": "/treva-firstReactWebsite/static/media/hotel.6b1f54a4.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/treva-firstReactWebsite/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "4ee21d04276bb6cd2ca5507534e43bd0",
+    "url": "/treva-firstReactWebsite/static/media/italy.4ee21d04.jpg"
+  },
+  {
+    "revision": "2fe1e1b8c39988279c64cd8440240a3b",
+    "url": "/treva-firstReactWebsite/static/media/kashmir.2fe1e1b8.jpg"
+  },
+  {
+    "revision": "a46c8f4570894a486a6764913b865c87",
+    "url": "/treva-firstReactWebsite/static/media/kerala.a46c8f45.jpg"
+  },
+  {
+    "revision": "d077471745bb122aebdd02c483893c86",
+    "url": "/treva-firstReactWebsite/static/media/logo.d0774717.svg"
+  },
+  {
+    "revision": "84b8743684e332364d0f4c83c1fdb231",
+    "url": "/treva-firstReactWebsite/static/media/maldives.84b87436.jpg"
+  },
+  {
+    "revision": "ece3a8aeb4c5f493aeb0cc0f4f7d1cdf",
+    "url": "/treva-firstReactWebsite/static/media/plane.ece3a8ae.svg"
+  },
+  {
+    "revision": "3328550138b7e46149ad5a8f35471d3e",
+    "url": "/treva-firstReactWebsite/static/media/tired.33285501.svg"
+  },
+  {
+    "revision": "4a7fa591c94d170d453d36b02ecf9da1",
+    "url": "/treva-firstReactWebsite/static/media/traveling.4a7fa591.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/treva-firstReactWebsite/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "7e092d58cae4ecb2fddf1515fe4564aa",
+    "url": "/treva-firstReactWebsite/static/media/venice.7e092d58.jpg"
+  },
+  {
+    "revision": "a51ab7ed010a415170cd93a2a29d96dd",
+    "url": "/treva-firstReactWebsite/static/media/youtube.a51ab7ed.svg"
+  }
+]);
